7fdead44257a487caa0bfe3c8963bb39ea14a5ea
Kurt Shian <fotock@users.noreply.github.com>
2020-08-11 17:47:29 +0800

    Update jpg2heic.sh

--- jpg2heic.sh
+++ jpg2heic.sh
@@ -6,8 +6,8 @@ for x in *.jpg; do
     exiftool -overwrite_original -tagsFromFile "$x" -all:all "${x%.jpg}.heic"
 
     XMP="${x%.jpg}.XMP"
-    if test -f "${XMP}"; then
-        exiftool -overwrite_original -tagsFromFile "${XMP}" -all:all "${x%.jpg}.heic"
+    if test -f "$XMP"; then
+        exiftool -overwrite_original -tagsFromFile "$XMP" -all:all "${x%.jpg}.heic"
     fi  
 done
 
@@ -18,7 +18,7 @@ for x in *.JPG; do
     exiftool -overwrite_original -tagsFromFile "$x" -all:all "${x%.JPG}.heic"
 
     XMP="${x%.JPG}.XMP"
-    if test -f "${XMP}"; then
-        exiftool -overwrite_original -tagsFromFile "${XMP}" -all:all "${x%.JPG}.heic"
+    if test -f "$XMP"; then
+        exiftool -overwrite_original -tagsFromFile "$XMP" -all:all "${x%.JPG}.heic"
     fi  
 done
